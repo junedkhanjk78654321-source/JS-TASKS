84edd8855608e19c0fd6a50c5e9ccdb71c4f361a
juned khan <junedkhanjk78654321@gmail.com>
2025-09-19 16:45:17 +0530

    range system

---
+++ range.js
@@ -0,0 +1,10 @@
+let input = prompt("enter the number")
+if( input <=10){
+    console.log("this is in range")
+}
+else if ( input >=50){
+    console.log("out of range")
+}
+else {
+    console.log("not valid")
+}
