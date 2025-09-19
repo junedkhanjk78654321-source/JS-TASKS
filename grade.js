fc13795e4f7008df51dbf490029a4926ecf96220
juned khan <junedkhanjk78654321@gmail.com>
2025-09-19 16:30:23 +0530

    grading system

---
+++ grade.js
@@ -0,0 +1,17 @@
+let input = prompt("enter the marks")
+if (input >= 90) {
+    console.log("A+")
+}
+else if (input >= 75) {
+    console.log("A")
+}
+else if (input >= 60) {
+    console.log("B")
+}
+else if (input >= 40) {
+    console.log("C")
+}
+else {
+console.log("fail")
+}
+// console.log("fail")
