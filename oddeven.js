f13569b49e25152a5d1df90af4c962c01b0265f0
juned khan <junedkhanjk78654321@gmail.com>
2025-10-01 16:25:58 +0530

    odd even program using function

---
+++ oddeven.js
@@ -0,0 +1,18 @@
+let num = prompt("enter a number")
+if(num % 2 === 0)
+{console.log(iseven())
+}
+else if(num % 2 !==0){
+    console.log(isodd())
+}
+
+
+
+
+function isodd(){
+    console.log("odd")
+}
+
+function iseven(){
+    console.log("even")
+}
